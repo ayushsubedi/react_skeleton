ee51f498193db837aac7b0390c2c7bfb7d8c249a
Ayush Subedi <ayush.subedi@gmail.com>
2019-02-28 19:24:10 +0545

    added prettier and changed formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>     
-            Learn 
-            Edit <code>src/App.js</code> and save to reload.
+          <p>
+            Learn Edit <code>src/App.js</code> and save to reload.
           </p>
         </header>
       </div>
